4fb2aaa23864760c68f30058d4c663a0ee227c03
Harry(JangNamKyu) <nkjang@coupang.com>
2020-01-26 14:02:03 +0900

    v1 - chapter02

---
+++ src/main/java/microservice/book/multiplication/service/MultiplicationService.java
@@ -0,0 +1,12 @@
+package microservice.book.multiplication.service;
+
+import microservice.book.multiplication.domain.Multiplication;
+
+public interface MultiplicationService {
+	/**
+	 * 두 개의 무작위 인수를 담은 {@link Multiplication} 객체를 생성한다.
+	 * 무작위로 생성되는 숫자의 범위는 11~99
+	 * @return 무작위 인수를 담은 {@link Multiplication} 객체
+	 */
+	Multiplication createRandomMultiplication();
+}
---
+++ src/test/java/microservice/book/multiplication/service/MultiplicationServiceTest.java
@@ -0,0 +1,37 @@
+package microservice.book.multiplication.service;
+
+import microservice.book.multiplication.domain.Multiplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultiplicationServiceTest {
+
+	@MockBean
+	private RandomGeneratorService randomGeneratorService;
+
+	@Autowired
+	private MultiplicationService multiplicationService;
+
+	@Test
+	public void crateRandomMultiplicationTest() {
+		// given (randomGeneratorService가 처음에 50, 나중에 30을 반환하도록 설정)
+		given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+
+		// when
+		Multiplication multiplication = multiplicationService.createRandomMultiplication();
+
+		// assert
+		assertThat(multiplication.getFactorA()).isEqualTo(50);
+		assertThat(multiplication.getFactorB()).isEqualTo(30);
+		assertThat(multiplication.getResult()).isEqualTo(1500);
+	}
+}
